0266404d5f09ffe94277d76c2d47068fc0e5944c
beBhavyhere <52847477+Bhavykapadiya@users.noreply.github.com>
2025-07-18 13:58:50 +0000

    Updated

--- api/chromium-test.js
+++ api/chromium-test.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer-core');
 const chromium = require('@sparticuz/chromium');
 
 module.exports = async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
+  console.log('chromium.executablePath:', chromium.executablePath);
   res.status(200).json({
     execPath: chromium.executablePath,
     nodeEnv: process.env.NODE_ENV,
