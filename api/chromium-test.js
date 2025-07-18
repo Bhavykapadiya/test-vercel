0389eb512ef0ecaee0510015b18ec927d7641540
beBhavyhere <52847477+Bhavykapadiya@users.noreply.github.com>
2025-07-18 14:24:57 +0000

    Updated

--- api/chromium-test.js
+++ api/chromium-test.js
@@ -1,10 +1,11 @@
 const puppeteer = require('puppeteer-core');
 const chromium = require('@sparticuz/chromium-min');
-
+const remoteExecutablePath = "https://github.com/Sparticuz/chromium/releases/download/v121.0.0/chromium-v121.0.0-pack.tar";
 module.exports = async (req, res) => {
-  console.log('chromium.executablePath:', await chromium.executablePath());
+
+  console.log('chromium.executablePath:', await chromium.executablePath(remoteExecutablePath));
   res.status(200).json({
-    execPath: await chromium.executablePath(),
+    execPath: await chromium.executablePath(remoteExecutablePath),
     nodeEnv: process.env.NODE_ENV,
     nodeVer: process.version,
     platform: process.platform
